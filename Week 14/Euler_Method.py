82e220a496749eba1952e89746a99edaffb3f32e
HenryHutch2001 <79278617+HenryHutch2001@users.noreply.github.com>
2023-02-03 11:53:04 +0000

    Defining the Euler step function

--- Week 14/Euler_Method.py
+++ Week 14/Euler_Method.py
@@ -1,1 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+h = 0.1
+"Defining the step size"
+
+f = lambda t, x:x
+
+def euler_step(xn,t,h):
+    xn1 = xn + h*f(t,xn)
+    print(xn1)
+
+euler_step(1,0,h)

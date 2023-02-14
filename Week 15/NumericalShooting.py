4edf610f1f1d0d632a43fc6988c8bdb792c6749d
HenryHutch2001 <79278617+HenryHutch2001@users.noreply.github.com>
2023-02-14 10:45:39 +0000

    Commenting and producing figs for q1

--- Week 15/NumericalShooting.py
+++ Week 15/NumericalShooting.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from scipy import integrate
 
 
-def ode(u,t):
+def ode(u,t): #Defining the ODE
     a = 1
     b = 0.4
     d = 0.1
@@ -13,14 +13,16 @@ def ode(u,t):
     dxdt = x*(1-x) -(a*x*y)/(d+x)
     dydt = (b*y)*(1 - y/x)
     return [dxdt, dydt]
-u0 = [5,5]
-t = np.linspace(0,200,200)
-x = integrate.odeint(ode,u0,t)
-print(x[:,0])
-print(len(t))
 
-plt.plot(t,x[:,0])
-plt.plot(t,x[:,1])
+u0 = [5,5] #Initial conditions, e.g number of prey/predators
+t = np.linspace(0,200,200) #Defining the time 
+x = integrate.odeint(ode,u0,t) #Using the integrate.ode function to solve the ODE
+
+plt.plot(t,x[:,0],label = "Predators") #Plotting the first column of the solution 
+plt.plot(t,x[:,1], label = "Prey") #Plotting the second column
+plt.xlabel('t')
+plt.ylabel('Population') #Graph stuff
+plt.legend()
 plt.show()
 
 

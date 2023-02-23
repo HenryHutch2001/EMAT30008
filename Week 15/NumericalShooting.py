30cad9fc94687c33fe05cfb923a5ba5eb8e366c9
HenryHutch2001 <79278617+HenryHutch2001@users.noreply.github.com>
2023-02-23 16:05:09 +0000

    editing

--- Week 15/NumericalShooting.py
+++ Week 15/NumericalShooting.py
@@ -10,13 +10,26 @@ b = 0.1
 d = 0.1
 # %%
 def ode(t,y,a,b,d): #Keeping t in in case our ode requires it
-    dx_dt = y[0]*(1-y[0]) - (a*y[0]*y[1])/(d+y[0])
+    dx_dt = y[0]*(1-y[0]) - (a*y[0]*y[1])/(d+y[0]) #Defining the differential equation
     dy_dt = b*y[1]*(1-(y[1]/y[0]))
-    return [dx_dt, dy_dt]
-
+    return [dx_dt, dy_dt] #Returns the value of the ODE
 # Solving the ode
-t_eval = np.linspace(0,100,1000)
-Solution = scipy.integrate.solve_ivp(ode,[0, 100],[0.5,0.5],t_eval=t_eval,args=[1,0.1,0.1], rtol = 1e-4)
+t_eval = np.linspace(0,100,1000) #defines the timeframe for which we simulate the ode between
+Solution = scipy.integrate.solve_ivp(ode,[0, 100],[0.5,0.5],t_eval=t_eval,args=[1,0.1,0.1],rtol = 1e-4)
+#Uses the solve_ivp function to solve the ODE with the first argument being the function for which we solve
+#The second being the time for which we solve between, the third being the initial conditions...
+# %%
+#Isolating a periodic orbit:
+#For x:
+XPer = 0.8
+tol = 1e-3
+for i in range(1,len(Solution.y[0])):
+    boob = abs(XPer - Solution.y[0][i])
+    if boob <= tol:
+        print(Solution.y[0][i])
+    else:
+        continue
+
 # %%
 #Plotting ODE
 plt.plot(Solution.t,Solution.y[0,:], label = 'X') #Plotting X value by extracting the first column in the solution
@@ -27,20 +40,15 @@ plt.show()
 #Plotting the phase portrait
 plt.plot(Solution.y[0,:],Solution.y[1,:]) #Plots the phase portrait, the value of X against Y
 plt.show()
-#We can see that the ODE eventually converges to a limit cycle  
-
+#We can see that the ODE eventually converges to a limit cycle where the values keep following the same cycle 
 # %%
-
 #Determining the appropriate phase conditions for a limit cycle: 
     #A limit cycle is a closed trajectory in phase space, lets say the initial x0 = 0.8, by looking 
     #at the graph we can see that the value will eventually converge.
-    
-    
     #So our conditions for our limit cycle are x(0)=0.8 (as an initial guess), as the x value in
     #the ODE will return to this value after a period
 
-    
-#To isolate a periodic orbit i'd just plot the X vs Y values between a certain period
+#To isolate a periodic orbit i'd just 
 
 def shooting(U): #Finds the points and time period of the limit cycle
     x0,y0,T = U #Extracting the constituent parts of the input for the shooting function

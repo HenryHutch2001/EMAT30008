6c6b8b7079f9a823cefe777e73602fa783a8359b
HenryHutch2001 <79278617+HenryHutch2001@users.noreply.github.com>
2023-03-01 16:53:38 +0000

    Adding more documentation

--- Week 16/My_Functions.py
+++ Week 16/My_Functions.py
@@ -107,9 +107,46 @@ def solve_toEU(f,x0,t1,t2,h):
         Value = euler_step(f,x[i-1],t[i-1],h)
         x = np.vstack([x, Value]) 
    return t,x 
-print(type(solve_toEU(ode,[1,1],0,1,0.1))) 
 # %%
 def rk_step(f,xn,t,h):
+    """
+    A function that uses the 4th order Runge-Kutta approximation method to find a single step of the solution
+
+    SYNTAX
+    ------
+    The function is called in the following way:
+
+    rk_step(f,xn,t,h);
+
+    WHERE:
+
+        f: f is the ODE you wish to approximate, the form of the ode
+        input should be as follows;
+
+            def PredPrey(t,x0): Defining the ode as a function with inputs t and x0, time and initial conditions
+            a = 1     |
+            b = 0.1   |  Defining any parameters inside the function handle, you could leave them out of the function but you
+            d = 0.1   |  would have to define them as arguments when calling the function
+            x = x0[0] Splitting the initial conditions array into its constituent parts
+            y = x0[1]
+            dx_dt = x*(1-x) - (a*x*y)/(d+x) #Defining the differential equations
+            dy_dt = b*y*(1-(y/x))
+            return [dx_dt, dy_dt] #Returns the value of the ode as an array 
+        
+        xn: xn is the value of the ODE for which you wish to iterate a step further, the form of this input should be as follows;
+
+            xn = [x1,x2,...,xi], where i is the number of dimensions the ode has
+
+        t: t is the time value for which you wish to approximate your ODE at, in the form of an integer or floating point number
+
+        h: h is the value of the timestep for which you want to evaluate the ODE after, a smaller step size results in a more
+        accurate approximation to the ODE
+
+    OUTPUT
+    ------
+
+    The rk_step function returns a numpy array containing an approximation to the ODE at a time t+h
+    """
     k1 = np.array(f(t,xn))
     k2 = np.array(f(t+h/2,xn + h*k1/2))
     k3 = np.array(f(t+h/2,xn+h*(k2/2)))
@@ -119,6 +156,46 @@ def rk_step(f,xn,t,h):
 "Creating the function for approximation using RK4 method"
 # %%
 def solve_toRK(f,x0,t1,t2,h):
+   """
+   A function that uses the 4th order Runge-Kutta method of approximation to estimate the values of an ODE between a given timespan
+
+   SYNTAX
+    ------
+    The function is called in the following way:
+
+    solve_toRK(f,x0,t1,t2,h);
+
+    WHERE:
+
+        f: f is the ODE you wish to approximate, the form of the ode
+        input should be as follows;
+
+            def PredPrey(t,x0): Defining the ode as a function with inputs t and x0, time and initial conditions
+            a = 1     |
+            b = 0.1   |  Defining any parameters inside the function handle, you could leave them out of the function but you
+            d = 0.1   |  would have to define them as arguments when calling the function
+            x = x0[0] Splitting the initial conditions array into its constituent parts
+            y = x0[1]
+            dx_dt = x*(1-x) - (a*x*y)/(d+x) #Defining the differential equations
+            dy_dt = b*y*(1-(y/x))
+            return [dx_dt, dy_dt] #Returns the value of the ode as an array 
+        
+        x0: x0 is the value of the ODE for which you wish to iterate from, it's initial conditions. The form of this input should be as follows;
+
+            x0 = [x1,x2,...,xi], where i is the number of dimensions the ode has
+
+        t1 & t2 : t1 & t2 are the time values for which you wish to approximate your ODE between, in the form of integers or floating point numbers
+
+        h: h is the value of the timestep for which you want to evaluate the ODE for, a smaller step size results in a more
+        accurate approximation to the ODE. If h is too large, the function will display an error message explaining that the
+        chosen stepsize is too large for an accurate approximation to the ODE.
+
+    OUTPUT
+    ------
+
+    The solve_toRK function returns a 2 values as a tuple. It returns the approximated values of the independent variables within the timespan
+    and the values for the time at which they were approximated. 
+   """
    deltat_max = 1
    if h >= deltat_max:
     print("Step size too large for accurate approximation")
@@ -131,6 +208,49 @@ def solve_toRK(f,x0,t1,t2,h):
    return t,x   
 # %%
 def solve_to(f,x0,t1,t2,h):
+    """
+   A function that uses methods of approximation to estimate the values of an ODE between a given timespan
+
+   SYNTAX
+    ------
+    The function is called in the following way:
+
+    solve_to(f,x0,t1,t2,h);
+
+    The function will display a message asking what approximation method the user would like to use, for which they enter either euler or runge-kutta.
+    If the user provides an incorrect input, the program will exit the function.
+
+    WHERE:
+
+        f: f is the ODE you wish to approximate, the form of the ode
+        input should be as follows;
+
+            def PredPrey(t,x0): Defining the ode as a function with inputs t and x0, time and initial conditions
+            a = 1     |
+            b = 0.1   |  Defining any parameters inside the function handle, you could leave them out of the function but you
+            d = 0.1   |  would have to define them as arguments when calling the function
+            x = x0[0] Splitting the initial conditions array into its constituent parts
+            y = x0[1]
+            dx_dt = x*(1-x) - (a*x*y)/(d+x) #Defining the differential equations
+            dy_dt = b*y*(1-(y/x))
+            return [dx_dt, dy_dt] #Returns the value of the ode as an array 
+        
+        x0: x0 is the value of the ODE for which you wish to iterate from, it's initial conditions. The form of this input should be as follows;
+
+            x0 = [x1,x2,...,xi], where i is the number of dimensions the ode has
+
+        t1 & t2 : t1 & t2 are the time values for which you wish to approximate your ODE between, in the form of integers or floating point numbers
+
+        h: h is the value of the timestep for which you want to evaluate the ODE for, a smaller step size results in a more
+        accurate approximation to the ODE. If h is too large, the function will display an error message explaining that the
+        chosen stepsize is too large for an accurate approximation to the ODE.
+
+    OUTPUT
+    ------
+
+    The solve_to function returns a 2 values as a tuple. It returns the approximated values of the independent variables within the timespan
+    and the values for the time at which they were approximated. 
+   """
     deltat_max = 1
     if h >= deltat_max:
         print("Step size too large for accurate approximation")
@@ -184,6 +304,4 @@ def shooting(x0,ode):
         return [*condition_1,condition_2]
     result = scipy.optimize.root(cons, x0 = x0, args=(ode,))
     print(result.x)
-
-
 # %%

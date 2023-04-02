7ec0dd153d09a7508505665c3c5094152214183d
HenryHutch2001 <79278617+HenryHutch2001@users.noreply.github.com>
2023-04-02 18:58:16 +0100

    Using my rk4 method to solve diffusion

--- RK4 PDE.py
+++ RK4 PDE.py
@@ -6,23 +6,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation 
 from My_Functions import solve_toRK
-# %% Discretising Space
-D = 0.5 # Defining the diffusion coefficient
-a = 0 # Defining the start of the domain 
-b = 1 # Defining the end of the domain
-alpha = 0 # Defining the boundary condition values for u(a,t) = alpha
-beta = 0 # Defining the boundary condition values for u(b,t) = beta
-N = 20 # Defining the number of gridpoints
-GridSpace = np.linspace(a,b,N+1) #Defining the gridspace
-x_int = GridSpace[1:-1] # Extracting the interior gridpoints from 'GridSpace'
-dx = (b-a)/N # Defining 'Delta x'
-C = 0.49 # C = dt*D/(dx)^2 must be =< 1/2
-dt = (C*dx**2)/D # Rearranging the equation for C gives the equation for the time step
-t_final = 1 # Final time in the simulation 
-N_t = ceil(t_final/dt) # Determine the number of timesteps 
-u = np.zeros((N_t+1,N-1))
+
+N = 20
+a = 0
+b = 1
+D = 1
+GridSpace = np.linspace(a,b,N)
+bc_left = 0
+bc_right = 0
+
+def IC(x):
+    return np.sin(np.pi*((x-a)/b-a))
+
+def CreateAandb(N,a,b, bc_left, bc_right):
+    A = np.zeros((N,N))
+    dx = (b-a)/N
+    for i in range(1,N-1):
+        A[i,i] = -2/((dx)**2)
+        A[i,i+1] = A[i,i-1] = 1/((dx)**2)
+    A[0,:] = [1] + [0]*(N-1)
+    A[N-1,:] = [0]*(N-1) + [1]
+    b = np.zeros(N)
+    b[0] = bc_left
+    b[N-1] = bc_right
+    return A,b, dx
+
+A_dd,b_dd,dx = CreateAandb(N,a,b,bc_left,bc_right)
+
+def IC(x):
+    return np.sin((np.pi*(x-a))/(b-a))
+
+def system(t,u,q=1):
+    du_dt = (D/(dx**2))*(A_dd*u+b_dd)
+    return du_dt
+
+x0 = IC(GridSpace)
+
+x = np.linspace(a,b,1000)
+t = 0
+def true_solution(x, t):
+    return np.exp(-(D*math.pi**2*t)/(b-a)**2) * np.sin((math.pi*(x-a))/(b-a))
+
+u_true = true_solution(x, t)
+
+# Plot the true solution
+plt.plot(x, u_true, label='True solution')
+
+t,x = solve_toRK(system,IC(GridSpace),0,10,0.1)
+# extract the solution values for each time step
+plt.plot(GridSpace,x[0,:],'o')
+plt.show()
 
 
 
 
 
+
+# %%

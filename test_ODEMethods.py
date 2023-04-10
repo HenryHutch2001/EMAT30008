f4b7e65d06eaa802f47606d04a74305c4b9a798e
HenryHutch2001 <79278617+HenryHutch2001@users.noreply.github.com>
2023-04-10 15:39:27 +0100

    Adding output type test

--- test_ODEMethods.py
+++ test_ODEMethods.py
@@ -8,5 +8,27 @@ from My_Functions import shooting,solve_to
 import unittest
 import pytest
 # %%
+import numpy as np
+from My_Functions import solve_toEU, solve_toRK
+
+def ode(t,y): 
+    dx_dt = y[1]
+    dy_dt = -y[0]
+    return [dx_dt, dy_dt]
+
+def test_EU():
+    output = solve_toEU(ode,[1,1],0,1,0.1)
+    assert isinstance(output, tuple)
+    with pytest.raises(ValueError):
+        solve_toEU(ode,[1,1],-1,1,0.1)
+    with pytest.raises(ValueError):
+        solve_toEU(ode,[1,1],0,1,0.5)
+    with pytest.raises(ValueError):
+        solve_toEU(ode,[1,1],1,0,0.5)
+
+def test_output_typeRK():
+    output = solve_toRK(ode,[1,1],0,1,0.1)
+    assert isinstance(output, tuple)
+  
 
 
